5138e4f2a9d39abfe06c7ebcb477b7def502b5c4
nicofighter45 <fagot.nicolas.45@gmail.com>
2026-06-03 16:41:13 +0200

    Show variables names and types

--- visualisation.py
+++ visualisation.py
@@ -1,9 +1,8 @@
 import pandas as pd
 
 def visualize(tables: dict[str, pd.DataFrame]) -> None:
-    for table_name, table_info in tables.items():
-        print(f"\nTable: {table_name}")
-        print("-" * 50)
-
-        print(table_info.head())
+    for table_name, data_frame in tables.items():
+        print(f"\nTable {table_name} variables:")
+        for var in data_frame.columns:
+            print(f"{var.split("/")[1]} ({data_frame[var].dtype})")
     
